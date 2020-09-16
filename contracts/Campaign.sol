f6e38b0f820583b789c946cb9b4b7f7c69624221
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-09-16 16:32:40 +0200

    imported contract from remix.ethereum.org

---
+++ contracts/Campaign.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+
+    uint numRequests = 0;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint minimum) {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers[msg.sender] = true;
+    }
+
+    function createRequest(string memory description, uint value, address recipient) public restricted {
+        requests.push();
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+    }
+
+}
