7ab32e276b623d05d10733ce7888a13ad0daf5d6
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-09-19 12:00:35 +0200

    beforeEach statement

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -6,8 +6,12 @@ const web3 = new Web3(ganache.provider());
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
 
-const interface = JSON.parse(compiledFactory).abi;
-const bytecode = JSON.parse(compiledFactory).evm.bytecode.object;
+const f_interface = JSON.parse(compiledFactory).abi;
+const f_bytecode = JSON.parse(compiledFactory).evm.bytecode.object;
+
+const c_interface = JSON.parse(compiledCampaign).abi;
+const c_bytecode = JSON.parse(compiledCampaign).evm.bytecode.object;
+
 
 let accounts;
 let factory;
@@ -17,7 +21,16 @@ let campaign;
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
-    factory = await new web3.eth.Contract(interface)
-        .deploy({ data: bytecode })
+    factory = await new web3.eth.Contract(f_interface)
+        .deploy({ data: f_bytecode })
         .send({ from: accounts[0], gas:'1000000' });
+
+    await factory.methods.createCampaign('100').send({
+        from: accounts[0],
+        gas: '1000000'
+    });
+
+    const addresses = await factory.methods.getDeployedCampaigns().call();
+    campaignAddress = addresses[0];
+    campaign = await new web3.eth.Contract(c_interface, campaignAddress);
 })
