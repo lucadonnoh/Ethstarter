031c99a2df47a9316bb76c4a276a8cc39b7fdb27
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-09-24 16:52:45 +0200

    factory instance file

---
+++ ethereum/factory.js
@@ -0,0 +1,12 @@
+import web3 from './web3';
+import CampaignFactory from './build/CampaignFactory.json';
+
+const instance = new web3.eth.Contract(
+    CampaignFactory.abi,
+    '0xB79100e30D6d09F1cae92483C1fb6789Fdf8F7a'
+//const f_bytecode = compiledFactory.evm.bytecode.object;
+
+
+);
+
+export default instance;
