f7ca04318f7b97061fbee91e2d93bc567552d78d
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-09-26 15:24:49 +0200

    fixed address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,10 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     CampaignFactory.abi,
-    '0xB79100e30D6d09F1cae92483C1fb6789Fdf8F7a'
-//const f_bytecode = compiledFactory.evm.bytecode.object;
-
-
+    '0xB79100e30D6d09F1cae92483C1fb6789Fdf8F7ac'
 );
 
 export default instance;
