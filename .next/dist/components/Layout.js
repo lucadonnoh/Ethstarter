47ac2ccde993079140031340b1ada3b3acd71053
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-09-27 16:25:07 +0200

    container

--- .next/dist/components/Layout.js
+++ .next/dist/components/Layout.js
@@ -8,6 +8,8 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _semanticUiReact = require('semantic-ui-react');
+
 var _Header = require('./Header');
 
 var _Header2 = _interopRequireDefault(_Header);
@@ -17,16 +19,16 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var _jsxFileName = 'C:\\Users\\donno\\Documents\\GitHub\\Ethstarter\\components\\Layout.js';
 
 exports.default = function (props) {
-    return _react2.default.createElement('div', {
-        __source: {
-            fileName: _jsxFileName,
-            lineNumber: 5
-        }
-    }, _react2.default.createElement(_Header2.default, {
+    return _react2.default.createElement(_semanticUiReact.Container, {
         __source: {
             fileName: _jsxFileName,
             lineNumber: 6
         }
+    }, _react2.default.createElement(_Header2.default, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 7
+        }
     }), props.children);
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXExheW91dC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkhlYWRlciIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFPLEFBQVksQUFDbkI7Ozs7Ozs7O2tCQUFlLGlCQUFTLEFBQ3BCOzJCQUNJLGNBQUE7O3NCQUFBO3dCQUFBLEFBQ0k7QUFESjtBQUFBLEtBQUEsa0JBQ0ksQUFBQzs7c0JBQUQ7d0JBREosQUFDSSxBQUNDO0FBREQ7QUFBQSxjQUZSLEFBQ0ksQUFFVyxBQUdsQjtBQVBEIiwiZmlsZSI6IkxheW91dC5qcyIsInNvdXJjZVJvb3QiOiJDOi9Vc2Vycy9kb25uby9Eb2N1bWVudHMvR2l0SHViL0V0aHN0YXJ0ZXIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXExheW91dC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkNvbnRhaW5lciIsIkhlYWRlciIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFTOztBQUNULEFBQU8sQUFBWSxBQUNuQjs7Ozs7Ozs7a0JBQWUsaUJBQVMsQUFDcEI7MkJBQ0ksQUFBQzs7c0JBQUQ7d0JBQUEsQUFDSTtBQURKO0FBQUEsS0FBQSxrQkFDSSxBQUFDOztzQkFBRDt3QkFESixBQUNJLEFBQ0M7QUFERDtBQUFBLGNBRlIsQUFDSSxBQUVXLEFBR2xCO0FBUEQiLCJmaWxlIjoiTGF5b3V0LmpzIiwic291cmNlUm9vdCI6IkM6L1VzZXJzL2Rvbm5vL0RvY3VtZW50cy9HaXRIdWIvRXRoc3RhcnRlciJ9
