e00e22c1460265b272dbc4c0f4ad1c0029ac960e
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-10-10 11:16:52 +0200

    create campaign form submit

---
+++ .next/5.23cd23be71fd062e6128.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate(5,{
+
+/***/ 1358:
+/***/ (function(module, exports) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: C:/Users/donno/Documents/GitHub/Ethstarter/pages/campaigns/new.js?entry: await is a reserved word (14:25)\n\n\u001b[0m \u001b[90m 12 | \u001b[39m       event\u001b[33m.\u001b[39mpreventDefault()\u001b[33m;\u001b[39m\n \u001b[90m 13 | \u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 14 | \u001b[39m        \u001b[36mconst\u001b[39m accounts \u001b[33m=\u001b[39m await web3\u001b[33m.\u001b[39meth\u001b[33m.\u001b[39mgetAccounts()\u001b[33m;\u001b[39m\n \u001b[90m    | \u001b[39m                         \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 15 | \u001b[39m        await factory\u001b[33m.\u001b[39mmethods\n \u001b[90m 16 | \u001b[39m        \u001b[33m.\u001b[39mcreateCampaign(\u001b[36mthis\u001b[39m\u001b[33m.\u001b[39mstate\u001b[33m.\u001b[39mminimumContribution)\n \u001b[90m 17 | \u001b[39m        \u001b[33m.\u001b[39msend({\u001b[0m\n");
+
+/***/ }),
+
+/***/ 83:
+false,
+
+/***/ 97:
+false,
+
+/***/ 98:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNS4yM2NkMjNiZTcxZmQwNjJlNjEyOC5ob3QtdXBkYXRlLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7O0EiLCJzb3VyY2VSb290IjoiIn0=
